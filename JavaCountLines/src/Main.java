a95d668f76876392dcf6af8ef77f8461b9df9561
Justin-Weich <92863010+1Houston1@users.noreply.github.com>
2023-11-23 13:18:16 +0100

    added function to read file line by line

--- JavaCountLines/src/Main.java
+++ JavaCountLines/src/Main.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.*;
@@ -8,8 +9,17 @@ import java.util.regex.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         assert(args.length == 2);
-        // Regular expression ".*\.c$" matches any c source files
-        System.out.println(countLinesInAllFiles(args[0],args[1]));
+        long startTime = System.currentTimeMillis();
+        // first param defines how many measurements should be taken (10 -> 10 times at a distance of 1 sec)
+        MemoryUsageMonitor monitor = new MemoryUsageMonitor(Integer.parseInt(args[0]));
+        Thread monitorThread = new Thread(monitor);
+        monitorThread.start();
+        //args[1]: name of the .txt file, here "large_text_file.txt", args[2] Regular expression, here: ".*\.txt$"
+        //the entire file is read in here, to change it to line based reading use the countLinesInAllFilesLineByLine method
+        System.out.println("Lines: " + countLinesInAllFilesCompleteFile(args[1],args[2]));
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Duration: " + duration);
     }
 
     public static long countLines(String fileName) throws IOException {
@@ -17,7 +27,7 @@ public class Main {
         return allLines.size();
     }
 
-    public static long countLinesInAllFiles(String folderPath, String regex) throws IOException {
+    public static long countLinesInAllFilesCompleteFile(String folderPath, String regex) throws IOException {
         Pattern pattern = Pattern.compile(regex);
         try (Stream<Path> paths = Files.walk(Paths.get(folderPath))) {
             return paths
@@ -33,6 +43,30 @@ public class Main {
                         }
                     })
                     .sum();
+        }
+    }
+
+    public static long countLinesInAllFilesLineByLine(String folderPath, String regex) throws IOException {
+        Pattern pattern = Pattern.compile(regex);
+
+        try (Stream<Path> paths = Files.walk(Paths.get(folderPath))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> pattern.matcher(path.toString()).matches())
+                    .mapToLong(path -> {
+                        try {
+                            return countLines(path);
+                        } catch (IOException e) {
+                            throw new UncheckedIOException(e);
+                        }
+                    })
+                    .sum();
+        }
+    }
+
+    private static long countLines(Path filePath) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            return reader.lines().count();
         }
     }
 }
